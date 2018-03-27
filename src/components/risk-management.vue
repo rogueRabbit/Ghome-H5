fec349ce411b0bfbef0feefd6efab1fe13c49433
潘登 <pandeng@pandengdeMacBook-Pro.local>
2018-03-27 13:53:21 +0800

    验证码

---
+++ src/components/risk-management.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "risk-management",
+        props: {
+          imagecodeType: {
+            type: Number,
+            required: true
+          },
+          checkCodeUrl: {
+            type: String,
+            required: true
+          }
+        },
+        methods: {
+
+
+
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
