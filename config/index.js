f47a5b47548d30aca53f7c57c9628a36eef20e63
wangpan.abel <wangpan.abel@shandagames.com>
2018-04-02 20:04:21 +0800

    静态资源

--- config/index.js
+++ config/index.js
@@ -63,7 +63,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/h5'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://mgame.sdo.com/h5/',
 
     /**
      * Source Maps
