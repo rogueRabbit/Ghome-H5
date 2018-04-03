2ffb318a4d94e427829a6bfaff3a5865405713e3
wangpan.abel <wangpan.abel@shandagames.com>
2018-04-03 20:21:56 +0800

    中转页

---
+++ src/app/game/game.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="demo">
+        111
+    </div>
+</template>
+
+<script>
+    import "./demo.scss";
+    import { APIs } from '@/api/requestUrl'
+    import { getPostData } from '@/api/ghhttp.js'
+    import Loading from '@/components/loading/'
+    /* eslint-disable */
+    export default {
+        name: "Game",
+        data() {
+            return {
+                
+            };
+        },
+        created: function () { },
+        ready() {
+        },
+        mounted: function () {
+           
+            Loading(
+                {
+                    message: '图片格式不支持',
+                    duration: 10000
+                }
+            );
+        },
+        methods: {
+            
+        }
+    };
+</script>
--- src/utils/Tools.js
+++ src/utils/Tools.js
@@ -11,6 +11,13 @@ const removeStorage =  function(name){
   localStorage.removeItem(name);
 };
 
+const getSessionStorage = function(name){
+  return JSON.parse(sessionStorage.getItem(name))
+};
+
+const setSessionStorage = function(name, val){
+  sessionStorage.setItem(name, JSON.stringify(val))
+};
 const isPoneAvailable =  function (str) {
   let myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
   if (!myreg.test(str)) {
@@ -24,6 +31,8 @@ export {
   getLocalStorage,
   setLocalStorage,
   isPoneAvailable,
-  removeStorage
+  removeStorage,
+  getSessionStorage,
+  setSessionStorage
 }
 
